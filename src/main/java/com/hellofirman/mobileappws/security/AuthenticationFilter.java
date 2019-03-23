faf7510e6bb9dba74d6c8c015e194e8f8ceffc15
mfirmanakbar <m.firman.aa@gmail.com>
2019-03-23 10:07:09 +0700

    S9L43 - S9L45. Dubug jsonwebtoken successfully

---
+++ src/main/java/com/hellofirman/mobileappws/security/AuthenticationFilter.java
@@ -0,0 +1,70 @@
+package com.hellofirman.mobileappws.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hellofirman.mobileappws.ui.model.request.UserLoginRequestModel;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+	private final AuthenticationManager authenticationManager;
+	
+	public AuthenticationFilter(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
+	
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
+			throws AuthenticationException {
+		try {
+			
+			UserLoginRequestModel creds = new ObjectMapper()
+					.readValue(req.getInputStream(), UserLoginRequestModel.class);
+			
+			return authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(
+							creds.getEmail(),
+							creds.getPassword(),
+							new ArrayList<>()
+					)
+			);
+			
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}		
+	}
+	
+	@Override
+	protected void successfulAuthentication(HttpServletRequest req, 
+			HttpServletResponse res, FilterChain chain, Authentication auth) 
+					throws IOException, ServletException {
+		
+		String userName = ((User)auth.getPrincipal()).getUsername();
+		
+		//already import in our pom.xml --> jsonwebtoken
+		String token = Jwts.builder()
+				.setSubject(userName)
+				.setExpiration(new Date(System.currentTimeMillis() + SecurityConstans.EXPIRATION_TIME))
+				.signWith(SignatureAlgorithm.HS512, SecurityConstans.TOKEN_SECRET)
+				.compact();
+		
+		res.addHeader(SecurityConstans.HEADER_STRING, SecurityConstans.TOKEN_PREFIX + token);
+	}
+	
+}
