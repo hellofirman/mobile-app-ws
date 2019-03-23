ddd8850027bdf60cf956626f5f35dc4d6d19dc3c
mfirmanakbar <m.firman.aa@gmail.com>
2019-03-23 10:56:58 +0700

    S9L47 - change url "login" become "/users/login"

--- src/main/java/com/hellofirman/mobileappws/security/WebSecurity.java
+++ src/main/java/com/hellofirman/mobileappws/security/WebSecurity.java
@@ -28,12 +28,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.POST, SecurityConstans.SIGN_UP_URL)
 		.permitAll()
 		.anyRequest()
-		.authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
+		.authenticated().and().addFilter(getAuthenticationFilter());
 	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	}
+	
+	public AuthenticationFilter getAuthenticationFilter() throws Exception{
+		final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+		filter.setFilterProcessesUrl("/users/login");
+		return filter;
 	}
 	
 }
