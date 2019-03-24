d7cff9d2fd56ceb4c27b7fd79593f16615980295
mfirmanakbar <m.firman.aa@gmail.com>
2019-03-24 16:28:50 +0700

    #S9l52 - #S9l55 - GET user by id
    Still Denied. i have no idea why record not showing

--- src/main/java/com/hellofirman/mobileappws/io/repositories/UserRepository.java
+++ src/main/java/com/hellofirman/mobileappws/io/repositories/UserRepository.java
@@ -7,7 +7,8 @@ import com.hellofirman.mobileappws.io.entity.UserEntity;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long>{
-	
 	UserEntity findUserByEmail(String email);
 	
+	// findBy (for select query) && UserId (name must same like user_id in database)
+	UserEntity findByUserId(String userId);
 }
--- src/main/java/com/hellofirman/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/hellofirman/mobileappws/ui/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,15 +18,20 @@ import com.hellofirman.mobileappws.ui.model.response.UserRest;
 
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("users")
 public class UserController {
 
 	@Autowired
 	UserService userService;
-	
-	@GetMapping
-	public String getUser() {
-		return "get users was called";
+		
+	@GetMapping(path="/{id}")
+	public UserRest getUser(@PathVariable String id) {
+		UserRest returnValue = new UserRest();
+		
+		UserDto userDto = userService.getUserByUserId(id);
+		BeanUtils.copyProperties(userDto, returnValue);
+		
+		return returnValue;
 	}
 	
 	@PostMapping
